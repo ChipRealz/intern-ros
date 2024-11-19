8368dcd1b655b2a002f3b67e339d51f02c22bc95
ChipRealz <21110247@student.hcmute.edu.vn>
2024-11-19 20:43:23 +0700

    change user model

--- database/user.model.ts
+++ database/user.model.ts
@@ -2,19 +2,19 @@ import {Schema, models, model, Document} from 'mongoose';
 
 export interface IUser extends Document {
     clerkId: string;
-    email: string;
     name: string;
     username: string;
+    email: string;
     password?: string;
     joinedAt: Date;
 }
 
 const UserSchema = new Schema({
     clerkId: { type: String, required: true },
-    email: { type: String, required: true },
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
     joinedAt: { type: Date, default: Date.now }
 });
 
