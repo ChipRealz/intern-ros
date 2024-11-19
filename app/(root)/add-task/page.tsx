93fdbd49835a05316b146c66baf0ba495dc8421b
ChipRealz <21110247@student.hcmute.edu.vn>
2024-11-19 21:26:21 +0700

    test

--- app/(root)/add-task/page.tsx
+++ app/(root)/add-task/page.tsx
@@ -1,14 +1,24 @@
 import Task from '@/components/Forms/Task'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { getUserById } from '@/lib/actions/user.action';
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const { userId } = await auth();
+
+
+  if (!userId) redirect('/sign-in');
+
+  const mongoUser = await getUserById({ userId });
+
   return (
     <div>
         <h1 className='font-bold text-black text-3xl'>
             Add Task
         </h1>
         <div>
-            <Task/>
+            <Task mongoUserId={JSON.stringify(mongoUser._id)}/>
         </div>
     </div>
   )
